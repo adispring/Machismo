649184a458d142a8a86de69351aa03955dc897ae
adispring <sunnyadi@163.com>
2014-11-24 15:28:15 +0800

    card game
    when flip, return random card of different suit and rank .

--- Machismo/ViewController.m
+++ Machismo/ViewController.m
@@ -7,14 +7,27 @@
 //
 
 #import "ViewController.h"
+#import "Deck.h"
+#import "PlayingCardDeck.h"
 
 @interface ViewController ()
 @property (weak, nonatomic) IBOutlet UILabel *flipsLabel;
 @property (nonatomic) int flipCount;
-
+@property (strong, nonatomic) Deck *deck;
 @end
 
 @implementation ViewController
+
+- (Deck *)deck{
+    if (!_deck) {
+        _deck = [self createDeck];
+    }
+    return _deck;
+}
+
+- (Deck *)createDeck {
+    return [[PlayingCardDeck alloc] init];
+}
 
 - (void)setFlipCount:(int)flipCount{
     _flipCount = flipCount;
@@ -29,9 +42,12 @@
         [sender setTitle:@"" forState:UIControlStateNormal];
 
     } else {
-        [sender setBackgroundImage:[UIImage imageNamed:@"cardfront"]
-                          forState:UIControlStateNormal];
-        [sender setTitle:@"A♣︎" forState:UIControlStateNormal];
+        Card *randomCard = [self.deck drawRandomCard];
+        if (randomCard) {
+            [sender setBackgroundImage:[UIImage imageNamed:@"cardfront"]
+                              forState:UIControlStateNormal];
+            [sender setTitle:randomCard.contents forState:UIControlStateNormal];            
+        }
 
     }
     self.flipCount++;
