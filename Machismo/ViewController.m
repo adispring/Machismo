af617a91a84c286b7b25d05edc48737c6fdc8248
adispring <sunnyadi@163.com>
2014-11-24 14:50:25 +0800

    Card Game,CS193p's Lecture 2
    The app can flip the card and display the flip count.

--- Machismo/ViewController.m
+++ Machismo/ViewController.m
@@ -9,12 +9,33 @@
 #import "ViewController.h"
 
 @interface ViewController ()
+@property (weak, nonatomic) IBOutlet UILabel *flipsLabel;
+@property (nonatomic) int flipCount;
 
 @end
 
 @implementation ViewController
 
+- (void)setFlipCount:(int)flipCount{
+    _flipCount = flipCount;
+    self.flipsLabel.text = [NSString stringWithFormat:@"Flips: %d", self.flipCount];
+    NSLog(@"flipCount changed to %d", self.flipCount);
+}
+
 - (IBAction)touchCardButton:(UIButton *)sender {
+    if ([sender.currentTitle length]) {
+        [sender setBackgroundImage:[UIImage imageNamed:@"cardback"]
+                          forState:UIControlStateNormal];
+        [sender setTitle:@"" forState:UIControlStateNormal];
+
+    } else {
+        [sender setBackgroundImage:[UIImage imageNamed:@"cardfront"]
+                          forState:UIControlStateNormal];
+        [sender setTitle:@"A♣︎" forState:UIControlStateNormal];
+
+    }
+    self.flipCount++;
+    
 }
 
 @end
